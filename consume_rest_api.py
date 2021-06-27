b83e66360e0f26e647508100842aa48ea3618f2b
Amrit Sri Kumar <56089940+hi4mamrit@users.noreply.github.com>
2021-06-27 22:20:51 +0530

    Tool to find unanswered python questions, stackOverflow
    REST API to find links of recently unanswered python/java/unix questions in stackOverflow
    
    #How to Call:-
    python consume_rest_api.py --language=python --que
    python consume_rest_api.py -l=unix -q
    python consume_rest_api.py -l=java -q
    python consume_rest_api.py --help

---
+++ consume_rest_api.py
@@ -0,0 +1,65 @@
+"""
+a REST API to find links of recently unanswered python questions in stackOverflow
+
+#how to Call:-
+python consume_rest_api.py --language=python --que
+python consume_rest_api.py -l=python -q
+python consume_rest_api.py -l=java -q
+python consume_rest_api.py --help
+"""
+
+import requests,argparse,sys
+
+g_language = ''
+g_api_link = "http://api.stackexchange.com"
+g_so_dict ={'que': '/2.2/questions?order=desc&sort=activity&site=stackoverflow'}
+
+
+def parse_arguments():
+    global g_language,g_api_link
+   
+    argparser = argparse.ArgumentParser(description='A tool to find links of recently unanswered python(default) questions in stackOverflow')    
+    argparser.add_argument('--language' ,'-l' , default='python' ,\
+                           help='e.g -l=python .Questions related to which language. Default is Python')
+    argparser.add_argument('--que', '-q', dest='i_que', action='store_true' ,help='Option to support questions only....Mandatory')    
+    v_args = argparser.parse_args()
+    
+    g_language=v_args.language
+    if v_args.i_que:
+        g_api_link = g_api_link + g_so_dict['que']
+    else:
+        print("Please select -q as well. Exiting ...\n")
+        sys.exit(1)
+
+
+def f_na_language(p_list,p_language):
+    #Takes list as input , iterates over it filtering 'interested' tags and resturns the list of links as result
+    
+    v_list= list()
+    v_language = p_language.lower()
+    
+    for item in p_list:
+        if not bool(item['is_answered']) and (v_language in [x.lower() for x in item['tags']]):
+            v_list.append(item['link'])
+    
+    return v_list
+
+
+def main():
+    #Main Program
+    
+    parse_arguments()    
+    print ("Connecting: " ,g_api_link)    
+    response = requests.get(g_api_link)
+    i_list = response.json()['items']    
+
+    print ("Established..\nParsing...\n")
+    print("Searching for tag: ", g_language)    
+    ans_list =f_na_language(i_list,g_language)
+
+    print('\n'+g_language.capitalize() ,'Un-answered links:-')
+    print(*ans_list,sep='\n')
+    
+    
+if __name__ == '__main__':
+    main()
